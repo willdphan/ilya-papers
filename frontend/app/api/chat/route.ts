bb96033320aa6cf324ffa7103bd07ab80c4b7a24
William Phan <willdphan@gmail.com>
2024-05-21 16:22:12 -0400

    fe editzs

--- frontend/app/api/chat/route.ts
+++ frontend/app/api/chat/route.ts
@@ -1,9 +1,3 @@
-import { OpenAI } from "openai";
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
 export const runtime = "edge";
 
 export async function POST(req: Request) {
